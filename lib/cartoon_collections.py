abf711afd8e61e18d16aba544a2240fcd667f8d5
Noah <noahhughesph@gmail.com>
2023-10-09 11:34:58 -0400

    all tests passing

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,24 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    _ = 1
+    for dwarf in dwarves:
+        print(f'{_}. {dwarf}')
+        _ += 1
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(calls):
+    return [call.title() + '!' for call in calls]
 
-def find_the_cheese():
-    pass
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if (len(call) > 4):
+            return True
+    return False
+
+
+def find_the_cheese(list_cheese):
+    cheeses = ['cheddar', 'gouda', 'camembert']
+    for cheese in list_cheese:
+        if (cheese in cheeses):
+            return cheese
+    return None
